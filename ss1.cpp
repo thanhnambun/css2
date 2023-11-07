2c6b123ffbd0fc4146d5442ed8c26b221612dfe8
thanhnambun <147359810+thanhnambun@users.noreply.github.com>
2023-11-08 01:29:52 +0700

    Add files via upload

---
+++ ss1.cpp
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(){
+	const float pi = 3.14;
+	float length, width, edge, radius ;
+	float perimeter, acreage;
+	perimeter = 2*pi*radius, [length + wigth] * 2, edge*4;
+	acreage = pi*radius*radius, edge*edge, length*wigth; 
+}
+
+ 
